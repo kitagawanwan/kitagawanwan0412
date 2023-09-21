0e309ae8962d44b05fc5ee18323314e0e143ccb5
kitagawanwan <144981066+kitagawanwan@users.noreply.github.com>
2023-09-21 23:53:02 +0000

    tg

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,6 @@ import streamlit as st
 import time
 import math
 
- 
-
 def factorize(n):
     factors = []
     while n % 2 == 0:
@@ -17,33 +15,22 @@ def factorize(n):
         factors.append(n)
     return factors
 
- 
-
 def main():
     st.title("素因数分解時間計測アプリ")
 
- 
-
     st.write("このアプリは、与えられた整数の素因数分解を行い、所要時間を計測します。")
-
- 
 
     number_to_factorize = st.number_input("素因数分解対象の数を入力してください:", min_value=1)
 
- 
-
     if st.button("計測開始"):
-        start_time = time.time()
-        factors = factorize(number_to_factorize)
-        end_time = time.time()
-        elapsed_time = end_time - start_time
-
- 
+        with st.spinner("計測中..."):
+            start_time = time.time()
+            factors = factorize(number_to_factorize)
+            end_time = time.time()
+            elapsed_time = end_time - start_time
 
         st.write(f"素因数分解結果: {factors}")
         st.write(f"素因数分解にかかる時間: {elapsed_time:.4f} 秒")
-
- 
 
 if __name__ == "__main__":
     main()
